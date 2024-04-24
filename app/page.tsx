913da61a19357387614fdb544e0e87a96ec88c85
asaad595 <saidnayanet@gmail.com>
2024-04-24 18:46:31 +0300

    add navbar and hero

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import ProjectCard from "@/components/ProjectCard";
+import RegisterForm from "@/components/RegisterForm";
+import React from "react";
+
+export default function Home() {
+  return (
+    <section className="min-h-screen flex justify-center items-center text-3xl">
+      
+        <h1 className="font-semibold">My-Madical App</h1>
+      
+    </section>
+  );
+}
